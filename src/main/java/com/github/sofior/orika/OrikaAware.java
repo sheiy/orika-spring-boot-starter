06b63aa43f964282be75d758da4cd5ab5fe6be43
Sofior <35429674@qq.com>
2018-07-11 14:06:00 +0800

    支持自定义mapperFactory

--- src/main/java/com/github/sofior/orika/OrikaAware.java
+++ src/main/java/com/github/sofior/orika/OrikaAware.java
@@ -3,7 +3,10 @@ package com.github.sofior.orika;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -14,13 +17,17 @@ import java.util.Map;
 @Configuration
 public class OrikaAware implements ApplicationContextAware {
 
+    private static final Logger logger = LoggerFactory.getLogger(OrikaAware.class);
+
     @Bean
     public MapperFacade mapperFacade() {
         return mapperFactory().getMapperFacade();
     }
 
     @Bean
+    @ConditionalOnMissingBean(MapperFactory.class)
     public MapperFactory mapperFactory() {
+        logger.debug("custom mapper factory not found use default");
         return new DefaultMapperFactory.Builder().build();
     }
 
